60e25435a9ab2d24eea900e395e401cc72461f08
Santino <santinopereyra39@gmail.com>
2026-04-06 11:56:12 -0300

    primeras modificaciones de los programas principales

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask, request, jsonify
+import db
+
+app = Flask(__name__)
+
+@app.route('/ranking', methods=['GET'])
+def ranking():
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
 
