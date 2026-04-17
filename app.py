ab43d7cf8e4b8ba5202fbdfff132d3965a0a6f46
cvnavarro06 <cvnavarro@fi.uba.ar>
2026-04-17 20:09:18 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,52 +1,10 @@
-import mysql.connector
 from flask import Flask, jsonify, request
-from db import get_db_connection
-
-
+from Routes.remplazar_datos_partidos import partidos_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(partidos_bp)
 
-
-@app.route('/Partidos/<int:id>/Resultados', methods=['PUT'])
-def remplazar_datos_partido(id):
-    #Abro conexión a la base de datos y creo un cursor para ejecutar consultas
-    datos = request.get_json() 
-    conexion = get_db_connection()
-    cursor = conexion.cursor(dictionary=True)    
-    cursor.execute("SELECT id FROM partidos WHERE id = %s", (id,))
-    #Tomo el primer valor consultado.
-    partido = cursor.fetchone()
-        
-    if not partido:
-            return jsonify({"code": 404, "description": f"El partido número {id} no existe"}), 404
-
-    try:
-        
-        if not datos or 'goles_local' not in datos or 'goles_visitante' not in datos:
-            return jsonify({"code": 400, "description": "Faltan campos obligatorios"}), 400
-    
-        if not isinstance(datos['goles_local'], int) or not isinstance(datos['goles_visitante'], int):
-            return jsonify({"code": 400, "description": "Los datos a ingresar deben de ser un número"}), 400
-        if(id < 0):
-            return jsonify({"code": 400, "description": f"El id:{id} no es un número válido "}), 400
-
-        query = "UPDATE partidos SET goles_local = %s, goles_visitante = %s WHERE id = %s"
-        datos_ingresados = (datos['goles_local'], datos['goles_visitante'], id)
-        
-        cursor.execute(query, datos_ingresados)
-        conexion.commit() #Guardo cambios en la base de datos
-
-        return jsonify({"code": 204, "description": ''}), 204
-    
-
-    except mysql.connector.Error as error:
-        print(f"Error de MySQL: {error}")
-        return jsonify({"code": 500, "description": "Error interno al acceder a la base de datos"}), 500
-
-    finally:
-        cursor.close()
-        conexion.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -1,20 +1,18 @@
 import mysql.connector
 
-with open('DB.sql', 'r') as file:
+with open('init.sql', 'r') as file:
     sql_script = file.read()
 
 conn = mysql.connector.connect(
     host='127.0.0.1',
     user='root',
-    password='123789', # La que pusiste en VS Code
-    database='Constantino' # El nombre de tu base de datos
-)
+    password='root', )
 
 
 cursor = conn.cursor()
 for statement in sql_script.split(';'):
     if statement.strip():
-        print(statement)  # Para que veas qué se está ejecutando
+        print(statement)
         cursor.execute(statement)
         conn.commit()
         print("Ejecutado con éxito")
