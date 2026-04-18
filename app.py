ce89326877cf4ee7bd3a07b29eff29d6511665c0
spereyra-spec2 <sdpereyra@fi.uba.ar>
2026-04-18 14:23:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from routes.partidos import partidos_bp
 from routes.usuarios import usuarios_bp
+from routes.ranking import ranking_bp
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(partidos_bp, url_prefix="/partidos")
 app.register_blueprint(usuarios_bp, url_prefix="/usuarios")
+app.register_blueprint(ranking_bp, url_prefix="/ranking")
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
