aa809ac74fd97858d743e6f1f923424fcf8e91c1
spereyra-spec2 <sdpereyra@fi.uba.ar>
2026-04-18 14:21:18 -0300

    Update db.py

--- db.py
+++ db.py
@@ -56,3 +56,31 @@ def delete_user(id): # Elimina los datos del usuario con el ID proporcionado par
             cur.close()
         if conn:
             conn.close()
+
+def obtener_ranking(limit, offset):
+    con = get_connection()
+    # x=1/0 #error intencional (500)
+    cursor = con.cursor(dictionary=True)
+    query = f"""
+        SELECT prediccion.id_usuario, 
+        SUM(
+            CASE 
+                WHEN prediccion.local = partidos.local AND prediccion.visitante = partidos.visitante THEN 3
+
+                WHEN SIGN(prediccion.local - prediccion.visitante) = SIGN(partidos.local - partidos.visitante) THEN 1
+
+                ELSE 0 
+            END
+        ) AS puntos
+    FROM prediccion
+    JOIN partidos ON prediccion.id_partido = partidos.id
+    GROUP BY prediccion.id_usuario
+    ORDER BY puntos DESC
+    LIMIT {limit} OFFSET {offset}
+    """
+    cursor.execute(query)
+    ranking = cursor.fetchall()
+    cursor.close()
+    con.close()
+    return ranking
+
